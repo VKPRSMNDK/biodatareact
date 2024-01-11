97b4183385fe5a9597be5bef3ef4ef1e319677c1
VKPRSMNDK <prasmandikaviki@gmail.com>
2024-01-11 15:24:16 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -4,20 +4,462 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <body>
+	
+          
+        
+		
+		
+		<header class="top-area">
+			<div class="header-area">
+				
+			    <nav class="navbar navbar-default bootsnav navbar-fixed dark no-background">
+
+			        <div class="container">
+
+			            
+			            <div class="navbar-header">
+			                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-menu">
+			                    <i class="fa fa-bars"></i>
+			                </button>
+			                <a class="navbar-brand" href="index.html">vkprsmndk</a>
+			            </div>
+			            <div class="collapse navbar-collapse menu-ui-design" id="navbar-menu">
+			                <ul class="nav navbar-nav navbar-right" data-in="fadeInDown" data-out="fadeOutUp">
+			                <li class=" smooth-menu active"></li>
+			                    <li class=" smooth-menu"><a href="#education">education</a></li>
+			                    <li class="smooth-menu"><a href="#skills">skills</a></li>
+			                    <li class="smooth-menu"><a href="#experience">experience</a></li>
+			                    
+			                </ul>
+			            </div>
+			        </div>
+			    </nav>
+			    
+			</div>
+		    <div class="clearfix"></div>
+
+		</header>
+    <section id="welcome-hero" class="welcome-hero">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12 text-center">
+						<div class="header-text">
+							<h2>hai <span>,</span>saya viki prasmandika <span>.</span></h2>
+							<p>desainer ui/ux dan pengembang web</p>
+							
+						</div>
+					</div>
+				</div>
+			</div>
+
+		</section>
+		
+		
+		<section id="about" class="about">
+			<div class="section-heading text-center">
+				<h2>about me</h2>
+			</div>
+			<div class="container">
+				<div class="about-content">
+					<div class="row">
+						<div class="col-sm-6">
+							<div class="single-about-txt">
+								<h3>  nama saya adalah viki prasmandika tinggal di desa bangsalsari kecamatan bangasalsari kabupaten jember
+                Saya seorang Desainer UI/UX Profesional dan pengembang Web. Dapat dilihat apakah saya telah mendapatkan elit, tetapi saya percaya bahwa saat seperti itu akan terjadi dengan banyak kerja keras dan penderitaan. Karena saya akan sampai pada detail terkecil tentang siapa kita
+								</h3>
+								<p>
+									Jangan marah karena sakitnya di teguran karena nikmatnya, ia ingin sehelai rambut dari kesakitannya dengan harapan tidak ada perkembangbiakan. Kecuali jika mereka dibutakan oleh hawa nafsu, maka mereka tidak akan keluar; yang salah adalah mereka yang meninggalkan kewajibannya dan melunakkan hatinya, itulah jerih payah. Tetapi agar saya dapat melihat dari mana semua kesalahan yang lahir ini menjadi kesenangan bagi mereka yang menuduh dan kepedihan bagi mereka yang memujinya, saya akan membuka seluruh permasalahannya, dan menjelaskan hal-hal yang dikatakan oleh penemu kebenaran itu dan, sebagaimana hal itu terjadi. adalah arsitek kehidupan yang bahagia. Karena tidak ada seorang pun yang mencemooh atau membenci atau lari dari kesenangan itu sendiri karena itu adalah kesenangan.
+								</p>
+								<div class="row">
+									<div class="col-sm-4">
+										<div class="single-about-add-info">
+											<h3>phone</h3>
+											<p>082331077980</p>
+										</div>
+									</div>
+									<div class="col-sm-4">
+										<div class="single-about-add-info">
+											<h3>email</h3>
+											<p>vkprsmndkgmail.com</p>
+										</div>
+									</div>
+									<div class="col-sm-4">
+										<div class="single-about-add-info">
+											<h3>istagram</h3>
+											<p>vkprsmndk</p>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="col-sm-offset-1 col-sm-5">
+							<div class="single-about-img">
+								<img src="browny-v1.0/assets/images/about/WhatsApp.jpeg" alt="profile_image" />
+								<div class="about-list-icon">
+									<ul>
+										<li>
+											<a href="#">
+												<i  class="fa fa-facebook" aria-hidden="true"></i>
+											</a>
+										</li>
+										<li>
+											<a href="#">
+												<i  class="fa fa-dribbble" aria-hidden="true"></i>
+											</a>
+											
+										</li>
+										<li>
+											<a href="#">
+												<i  class="fa fa-twitter" aria-hidden="true"></i>
+											</a>
+											
+										</li>
+										<li>
+											<a href="#">
+												<i  class="fa fa-linkedin" aria-hidden="true"></i>
+											</a>
+										</li>
+										<li>
+											<a href="#">
+												<i  class="fa fa-instagram" aria-hidden="true"></i>
+											</a>
+										</li>
+										
+										
+									</ul>
+								</div>
+
+							</div>
+
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		
+		<section id="education" class="education">
+			<div class="section-heading text-center">
+				<h2>education</h2>
+			</div>
+			<div class="container">
+				<div class="education-horizontal-timeline">
+					<div class="row">
+						<div class="col-sm-4">
+							<div class="single-horizontal-timeline">
+								<div class="experience-time">
+									<h2>2010 - 2016</h2>
+									<h3>SDN <span>01 </span> BANGASALLSARI</h3>
+								</div>
+								<div class="timeline-horizontal-border">
+									<i class="fa fa-circle" aria-hidden="true"></i>
+									<span class="single-timeline-horizontal"></span>
+								</div>
+								<div class="timeline">
+									<div class="timeline-content">
+										<h4 class="title">
+											JEMBER, JAWA TIMUR
+										</h4>
+										<h5>Kecamatan Bangsalsari</h5>
+										<p class="description">
+                    Memperoleh pendidikan merupakan langkah awal menuju sukses, yang tentunya selaras dengan cara berpikir yang terbuka
+										</p>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="col-sm-4">
+							<div class="single-horizontal-timeline">
+								<div class="experience-time">
+									<h2>2017 - 2019</h2>
+									<h3>SMP <span>AHLUL </span> IRFAN</h3>
+								</div>
+								<div class="timeline-horizontal-border">
+									<i class="fa fa-circle" aria-hidden="true"></i>
+									<span class="single-timeline-horizontal"></span>
+								</div>
+								<div class="timeline">
+									<div class="timeline-content">
+										<h4 class="title">
+                    JEMBER, JAWA TIMUR
+										</h4>
+										<h5>Berada di desa langkap</h5>
+										<p class="description">
+                    Belajar adalah kunci untuk membuka pintu kesuksesan. Jadikan setiap pelajaran sebagai kesempatan untuk tumbuh dan berkembang.
+										</p>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="col-sm-4">
+							<div class="single-horizontal-timeline">
+								<div class="experience-time">
+									<h2>2020 - 2023</h2>
+									<h3>SMKN <span>06</span> JEMBER</h3>
+								</div>
+								<div class="timeline-horizontal-border">
+									<i class="fa fa-circle" aria-hidden="true"></i>
+									<span class="single-timeline-horizontal spacial-horizontal-line
+									"></span>
+								</div>
+								<div class="timeline">
+									<div class="timeline-content">
+										<h4 class="title">
+											JEMBER, JAWA TIMUR
+										</h4>
+										<h5> TANGGUL-JEMBER</h5>
+										<p class="description">
+											saya berada di jurusan Rekayasa PERANGKAT LUNAK 
+										</p>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+		</section>
+		
+		<section id="skills" class="skills">
+				<div class="skill-content">
+					<div class="section-heading text-center">
+						<h2>skills</h2>
+					</div>
+					<div class="container">
+						<div class="row">
+							
+							<div class="col-md-12">
+								<div class="single-skill-content">
+									<div class="barWrapper">
+										<span class="progressText">html 5</span>
+										<div class="single-progress-txt">
+											<div class="progress ">
+												<div class="progress-bar" role="progressbar" aria-valuenow="90" aria-valuemin="10" aria-valuemax="100">
+													
+												</div>
+											</div>
+											<h3>90%</h3>	
+										</div>
+									</div>
+									<div class="barWrapper">
+										<span class="progressText">css 3 animation</span>
+										<div class="single-progress-txt">
+											<div class="progress ">
+											   <div class="progress-bar" role="progressbar" aria-valuenow="85" aria-valuemin="10" aria-valuemax="100">
+												    
+											   </div>
+											</div>
+											<h3>85%</h3>	
+										</div>
+									</div>
+									<div class="barWrapper">
+										<span class="progressText">communication</span>
+										<div class="single-progress-txt">
+											<div class="progress ">
+											   <div class="progress-bar" role="progressbar" aria-valuenow="97" aria-valuemin="10" aria-valuemax="100">
+												   
+											   </div>
+											</div>
+											<h3>97%</h3>	
+										</div>
+									</div>
+									<div class="barWrapper">
+										<span class="progressText"> creativity</span>
+										<div class="single-progress-txt">
+											<div class="progress ">
+											   <div class="progress-bar" role="progressbar" aria-valuenow="90" aria-valuemin="10" aria-valuemax="100">
+												    
+											   </div>
+											</div>
+											<h3>90%</h3>	
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>		
+				</div>
+		</section>
+		
+		
+		<section id="experience" class="experience">
+			<div class="section-heading text-center">
+				<h2>experience</h2>
+			</div>
+			<div class="container">
+				<div class="experience-content">
+						<div class="main-timeline">
+							<ul>
+								<li>
+									<div class="single-timeline-box fix">
+										<div class="row">
+											<div class="col-md-5">
+												<div class="experience-time text-right">
+													<h2>2017 - SMP</h2>
+													<h3>performance</h3>
+												</div>
+											</div>
+											<div class="col-md-offset-1 col-md-5">
+												<div class="timeline">
+													<div class="timeline-content">
+														<h4 class="title">
+															<span><i class="fa fa-circle" aria-hidden="true"></i></span>
+															SMP sekaligus Pondok Pesantren 
+														</h4>
+														<h5>Langkap- Bangsalsari</h5>
+														<p class="description">
+                            received achievements and also participated in science competitions in Jember
+														</p>
+													</div>
+												</div>
+											</div>
+										</div>
+									</div>
+								</li>
+
+								<li>
+									<div class="single-timeline-box fix">
+										<div class="row">
+											<div class="col-md-offset-1 col-md-5 experience-time-responsive">
+												<div class="experience-time">
+													<h2>
+														<span><i class="fa fa-circle" aria-hidden="true"></i></span>
+														2020 - 2021
+													</h2>
+													<h3>Organisai</h3>
+												</div>
+											</div>
+											<div class="col-md-5">
+												<div class="timeline">
+													<div class="timeline-content text-right">
+														<h4 class="title">
+															SMKN 06 JEMBER
+														</h4>
+														<h5>Tanggul,jember</h5>
+														<p class="description">
+                            was once the vice president of the student council
+
+ 
+														</p>
+													</div>
+												</div>
+											</div>
+											<div class="col-md-offset-1 col-md-5 experience-time-main">
+												<div class="experience-time">
+													<h2>
+														<span><i class="fa fa-circle" aria-hidden="true"></i></span>
+														2020 - 2021
+													</h2>
+													<h3>ORGANISASI</h3>
+												</div>
+											</div>
+										</div>
+									</div>
+								</li>
+
+								<li>
+									<div class="single-timeline-box fix">
+										<div class="row">
+											<div class="col-md-5">
+												<div class="experience-time text-right">
+													<h2>2022 - 2023</h2>
+													<h3>extracurricular</h3>
+												</div>
+											</div>
+											<div class="col-md-offset-1 col-md-5">
+												<div class="timeline">
+													<div class="timeline-content">
+														<h4 class="title">
+															<span><i class="fa fa-circle" aria-hidden="true"></i></span>
+															PASKIBRAKA
+														</h4>
+														<h5>Paskanam,Jember</h5>
+														<p class="description">
+                            became the general chairman of the paskibra
+														</p>
+													</div>
+												</div>
+											</div>
+										</div>
+									</div>
+								</li>
+
+								<li>
+									<div class="single-timeline-box fix">
+										<div class="row">
+											<div class="col-md-offset-1 col-md-5 experience-time-responsive">
+												<div class="experience-time">
+													<h2>
+														<span><i class="fa fa-circle" aria-hidden="true"></i></span>
+														2022 - 2023
+													</h2>
+													<h3>CO KEPEMIMPINAN</h3>
+												</div>
+											</div>
+											<div class="col-md-5">
+												<div class="timeline">
+													<div class="timeline-content text-right">
+														<h4 class="title">
+															OSKANER
+														</h4>
+														<h5>SMKN 06 JEMBER</h5>
+														<p class="description">
+                            train leadership skills and train school ceremony officials
+														</p>
+													</div>
+												</div>
+											</div>
+											<div class="col-md-offset-1 col-md-5 experience-time-main">
+												<div class="experience-time">
+													<h2>
+														<span><i class="fa fa-circle" aria-hidden="true"></i></span>
+														2022 - 2023
+													</h2>
+													<h3>CO KEPEMIMPINAN</h3>
+												</div>
+											</div>
+										</div>
+									</div>
+								</li>
+
+								<li>
+									<div class="single-timeline-box fix">
+										<div class="row">
+											<div class="col-md-5">
+												<div class="experience-time text-right">
+													<h2>2024</h2>
+													<h3>Kesuksesan</h3>
+												</div>
+											</div>
+											<div class="col-md-offset-1 col-md-5">
+												<div class="timeline">
+													<div class="timeline-content">
+														<h4 class="title">
+															<span><i class="fa fa-circle" aria-hidden="true"></i></span>
+															lebih banyak pengalaman
+														</h4>
+														
+														<p class="description">
+															kesuksesan, lebih baik, banyak keinginan nambah pengalaman 
+														</p>
+													</div>
+												</div>
+											</div>
+										</div>
+									</div>
+								</li>
+
+							</ul>
+						</div>
+					</div>
+			</div>
+
+		</section>
+		
+
+        
+    </body>
     </div>
   );
 }
